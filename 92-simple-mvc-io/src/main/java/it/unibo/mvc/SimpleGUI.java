507d5c6ac3c1c9355d84ea0e551eae08a69d61fe
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2024-11-19 16:57:34 +0100

    In exercise 92 in class SimpleGUI created a main, method display and field frame

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -10,4 +10,12 @@ public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
 
+    private void display() {
+        
+    }
+
+    public static void main(final String... args) {
+        new SimpleGUI().display();
+     }
+
 }
