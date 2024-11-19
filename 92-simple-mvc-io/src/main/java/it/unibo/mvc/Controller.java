a6b291a4869076b0437b53d94b0a3885687dcfc4
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2024-11-19 11:07:31 +0100

    In exercise 92 in class Controller created method setFile to assign at field file a file

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,16 @@
 package it.unibo.mvc;
 
+import java.io.File;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+
+    private File file;
+
+    public void setFile(final File file) {
+        this.file = file;
+    }
 
 }
