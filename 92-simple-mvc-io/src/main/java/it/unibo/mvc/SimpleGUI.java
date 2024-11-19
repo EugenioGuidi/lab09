022cccb5e3e6cac131f8c7f7bafd9fced528aeef
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2024-11-19 17:53:28 +0100

    In exercise 92 in class SimpleGUI created field controller (use it later) and added a new scrollable JTextArea

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,12 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
@@ -12,9 +18,18 @@ public final class SimpleGUI {
     private static final String TITLE = "MINE SIMPLE GUI APPLICATION";
     private static final int PROPORTION = 3;
     private final JFrame frame = new JFrame(TITLE);
+    private final Controller controller;
 
-    public SimpleGUI() {
-        
+    public SimpleGUI(Controller controller) {
+        this.controller = controller;
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+        final JButton save = new JButton("Save");
+        canvas.add(save, BorderLayout.SOUTH);
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final JTextArea textArea = new JTextArea();
+        canvas.add(new JScrollPane(textArea), BorderLayout.CENTER);
     }
 
     private void display() {
@@ -27,7 +42,8 @@ public final class SimpleGUI {
     }
 
     public static void main(final String... args) {
-        new SimpleGUI().display();
+        Controller controller = new Controller();
+        new SimpleGUI(controller).display();
      }
 
 }
