c3ec8e25617ddc3a04d059b04b53b2377a7188dc
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2024-11-19 11:28:33 +0100

    In exercise 92 in class Controller created method writeStringInFile that write in file, throwing IOexception in case

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,6 +1,9 @@
 package it.unibo.mvc;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Application controller. Performs the I/O.
@@ -19,6 +22,14 @@ public class Controller {
 
     public String getFilePath() {
         return this.file.getAbsolutePath();
+    }
+
+    public void setStringInFile (final String string) throws IOException {
+        try(BufferedWriter wr = new BufferedWriter(new FileWriter(this.file))) {
+            wr.write(string);
+        } catch (IOException e) {
+            throw new IOException("Error writing on file", e);
+        }
     }
 
 }
