c0862838edab5126e84b8edf18c22252e2ab032d
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2024-11-19 17:21:02 +0100

    In exercise 92 in class SimpleGUI implemented body of method display

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,17 +1,25 @@
 package it.unibo.mvc;
 
 import javax.swing.JFrame;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 /**
  * A very simple program using a graphical interface.
  * 
  */
 public final class SimpleGUI {
-
+    private static final String TITLE = "A very simple GUI application";
+    private static final int PROPORTION = 3;
     private final JFrame frame = new JFrame();
 
     private void display() {
-        
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
     }
 
     public static void main(final String... args) {
