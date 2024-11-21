1472c15efeb556cf8fd8b883cc86d43c198d5811
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2024-11-21 16:51:28 +0100

    In exercise 93 Implemented interface Controller with method declaretion

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,38 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
- *
+ * This interface has methods thatt model a controller
+ * for save and print string in standard output.
  */
 public interface Controller {
+
+    /**
+     * This methode set then next string to print.
+     * 
+     * @throws IllegalArgumentException if string is null
+     */
+    public void setString();
+
+    /**
+     * This method return the next string to print.
+     * 
+     * @return string
+     */
+    public String getString();
+
+    /**
+     * This method return the list of string.
+     * 
+     * @return listOfPrintedString
+     */
+    public List<String> getListOfPrintedString();
+
+    /**
+     * This method print current string
+     * @throws IllegalStateException if the current string is unset
+     */
+    public void printCurrentString();
 
 }
